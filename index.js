82be2784fab2a63f42c451e8c069cf0f4dc2d23d
Chris Nunez <cenunez47@gmail.com>
2019-01-18 23:00:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const app = "I don't do much.";
 
+const token = '019e4243e9b28e20a7e264f8ec831b58b617b23b'
 fetch('https://api.github.com/repos/jquery/jquery/commits')
   .then(res => res.json())
   .then(json => console.log(json));
