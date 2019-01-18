00cdac08dfa23eaf7017292fb3c0eb5ef2554dbf
Chris Nunez <cenunez47@gmail.com>
2019-01-18 23:03:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const app = "I don't do much.";
 
 const token = '019e4243e9b28e20a7e264f8ec831b58b617b23b'
-fetch('https://api.github.com/user/repos', {
-  headers: {
-    Authorization: `token ${token}`
-  }
-})
+
+fetch('https://api.github.com/user/repos')
   .then(res => res.json())
   .then(json => console.log(json));
