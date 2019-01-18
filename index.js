bf0b2156b78d139f648733191b169969463eb91c
Chris Nunez <cenunez47@gmail.com>
2019-01-18 23:02:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = "I don't do much.";
 
 const token = '019e4243e9b28e20a7e264f8ec831b58b617b23b'
-fetch('https://api.github.com/repos/jquery/jquery/commits', {
+fetch('https://api.github.com/user/repos', {
   headers: {
     Authorization: `token ${token}`
   }
